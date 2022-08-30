22ffb6f65c92e6a3d7bde094d4a089bf3882e89c
denisjosemendozavidal <denismendozavidal@gmail.com>
2022-08-29 22:17:14 -0500

    Adding selected products

---
+++ main.js
@@ -0,0 +1,7 @@
+function getToselected() {    
+    let pic = document.getElementById("selected-product").src = document.getElementById("product1").src;
+}
+
+function getToselected2() {    
+    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
+}
