bbc5114de13c31b2741a4f5378efef5a57396542
denisjosemendozavidal <denismendozavidal@gmail.com>
2022-09-03 12:58:06 -0500

    Adding effects

--- main.js
+++ main.js
@@ -1,91 +1,160 @@
-// Adding to selected product from product list.
 
-function getToselected() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product1").src;
+//the burger
+
+const Burger = document.querySelector("#Burger");
+const Nav = document.querySelector("#nav-elements-grandpa");
+
+Burger.addEventListener("click", function () {
+    Nav.classList.toggle("nav-elements-grandpa-show");
+});
+
+//Slider
+
+var counter = 1;
+      setInterval(function(){
+        document.getElementById('radio' + counter).checked = true;
+        counter++;
+        if(counter > 4){
+          counter = 1;
+        }
+      }, 3000);
+
+
+
+
+
+// Adding to selected product from product list.
+/*
+function getToselected(e) {    
+document.getElementById("selected-product").src = e.target.src;
 }
 
 function getToselected2() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
+    document.getElementById("selected-product").src = document.getElementById("product2").src;
 }
 
 function getToselected3() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product3").src;
+    document.getElementById("selected-product").src = document.getElementById("product3").src;
 }
 
 function getToselected4() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product4").src;
+     document.getElementById("selected-product").src = document.getElementById("product4").src;
 }
 
 function getToselected5() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product5").src;
+    document.getElementById("selected-product").src = document.getElementById("product5").src;
 }
 
 function getToselected6() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product6").src;
+    document.getElementById("selected-product").src = document.getElementById("product6").src;
 }
 
 function getToselected7() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product7").src;
+    document.getElementById("selected-product").src = document.getElementById("product7").src;
 }
 
 function getToselected8() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product8").src;
+    document.getElementById("selected-product").src = document.getElementById("product8").src;
 }
 
 function getToselected9() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product9").src;
+    document.getElementById("selected-product").src = document.getElementById("product9").src;
 }
 
 function getToselected10() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product10").src;
+    document.getElementById("selected-product").src = document.getElementById("product10").src;
 }
 
 function getToselected11() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product11").src;
+     document.getElementById("selected-product").src = document.getElementById("product11").src;
 }
 
 function getToselected12() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product12").src;
+    document.getElementById("selected-product").src = document.getElementById("product12").src;
 }
 
 function getToselected13() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product13").src;
+     document.getElementById("selected-product").src = document.getElementById("product13").src;
 }
 
 function getToselected14() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product14").src;
+     document.getElementById("selected-product").src = document.getElementById("product14").src;
 }
 
 function getToselected15() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product15").src;
+     document.getElementById("selected-product").src = document.getElementById("product15").src;
 }
 
 function getToselected16() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product16").src;
+     document.getElementById("selected-product").src = document.getElementById("product16").src;
 }
 
 function getToselected17() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product17").src;
+     document.getElementById("selected-product").src = document.getElementById("product17").src;
 }
 
 function getToselected18() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product18").src;
+     document.getElementById("selected-product").src = document.getElementById("product18").src;
 }
 
 function getToselected19() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product19").src;
+     document.getElementById("selected-product").src = document.getElementById("product19").src;
 }
 
 function getToselected20() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product20").src;
+     document.getElementById("selected-product").src = document.getElementById("product20").src;
 }
 
 function getToselected21() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product21").src;
+     document.getElementById("selected-product").src = document.getElementById("product21").src;
 }
 
 function getToselected22() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product22").src;
+     document.getElementById("selected-product").src = document.getElementById("product22").src;
+}
+
+function getToselected23() {    
+     document.getElementById("selected-product").src = document.getElementById("product2").src;
+}
+
+function getToselected() {    
+     document.getElementById("selected-product").src = document.getElementById("product1").src;
+}
+
+function getToselected2() {    
+     document.getElementById("selected-product").src = document.getElementById("product2").src;
+}
+
+function getToselected2() {    
+     document.getElementById("selected-product").src = document.getElementById("product2").src;
+}
+
+function getToselected2() {    
+     document.getElementById("selected-product").src = document.getElementById("product2").src;
+}
+
+function getToselected2() {    
+     document.getElementById("selected-product").src = document.getElementById("product2").src;
+}
+
+function getToselected2() {    
+    document.getElementById("selected-product").src = document.getElementById("product2").src;
+}
+
+function getToselected2() {    
+    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
+}
+
+function getToselected2() {    
+    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
+}
+
+function getToselected2() {    
+    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
+}
+
+function getToselected2() {    
+    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
 }
 
 function getToselected2() {    
@@ -180,46 +249,13 @@ function getToselected2() {
     let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
 }
 
-function getToselected() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product1").src;
+
+//Customizing the filters, as an Idea feeling like using toggle and match them based on their ATL
+
+function filters(filter) {
+    if (products[i].atl === filter.alt) {
+        
+    }
 }
 
-function getToselected2() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
-}
-
-function getToselected2() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
-}
-
-function getToselected2() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
-}
-
-function getToselected2() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
-}
-
-function getToselected2() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
-}
-
-function getToselected2() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
-}
-
-function getToselected2() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
-}
-
-function getToselected2() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
-}
-
-function getToselected2() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
-}
-
-function getToselected2() {    
-    let pic = document.getElementById("selected-product").src = document.getElementById("product2").src;
-}
+*/
